c947f7d717ed9bebc221c7246c5fd682199e85f0
Anil Sethy <anilsethy470@gmail.com>
2026-07-29 20:18:37 +0530

    Time: 177 ms (77.68%), Space: 25.7 MB (84.86%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,18 +1,17 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         left = 0
-        freq = {}
+        dic = {}
         res = -1
-
         for right in range(len(fruits)):
-            freq[fruits[right]] = freq.get(fruits[right], 0) + 1
+            dic[fruits[right]] = dic.get(fruits[right],0)+1
+            while(len(dic)>2):
+                dic[fruits[left]]-=1
+                if dic[fruits[left]]==0:
+                    del dic[fruits[left]]
+                left+=1
 
-            while len(freq) > 2:
-                freq[fruits[left]] -= 1
-                if freq[fruits[left]] == 0:
-                    del freq[fruits[left]]
-                left += 1
-
-            res = max(res, right - left + 1)
-
+            res = max(res,right-left+1)
         return res
+
+        
