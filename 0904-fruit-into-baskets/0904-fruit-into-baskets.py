062143125c71a46f2147fc6281539b8faad6be25
Anil Sethy <anilsethy470@gmail.com>
2026-07-29 20:11:49 +0530

    Time: 190 ms (48.06%), Space: 26.2 MB (13.03%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -2,20 +2,17 @@ class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         left = 0
         freq = {}
-        max_len = 0
+        res = 0
 
         for right in range(len(fruits)):
-            fruit = fruits[right]
-            freq[fruit] = freq.get(fruit, 0) + 1
+            freq[fruits[right]] = freq.get(fruits[right], 0) + 1
 
-            # shrink window if more than 2 fruit types
             while len(freq) > 2:
                 freq[fruits[left]] -= 1
                 if freq[fruits[left]] == 0:
                     del freq[fruits[left]]
                 left += 1
 
-            max_len = max(max_len, right - left + 1)
+            res = max(res, right - left + 1)
 
-        return max_len
-        
+        return res
