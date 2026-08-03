33a7f67c41679ee458925dafc178172a6e7f7116
Anil Sethy <anilsethy470@gmail.com>
2026-08-03 12:14:43 +0530

    Time: 1318 ms (5%), Space: 19.8 MB (36.85%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,30 +2,28 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         low = 0
         res = float('inf')
-        freq = [0] * 256
-        needed = [0] * 256
+        freq = [0]*256
+        nedded = [0]*256
         start = 0
-
-        def check(freq, needed):
+        def check(freq,nedded):
             for i in range(256):
-                if freq[i] < needed[i]:
+                if freq[i]<nedded[i]:
                     return False
             return True
-
         for ch in t:
-            needed[ord(ch)] += 1
-
+            nedded[ord(ch)]+=1
         for high in range(len(s)):
-            freq[ord(s[high])] += 1
+            freq[ord(s[high])]+=1
 
-            while check(freq, needed):
-                size = high - low + 1
-
-                if res > size:
+            while(check(freq,nedded)):
+                size = high-low+1
+                if res>size:
                     res = size
                     start = low
+                freq[ord(s[low])]-=1
+                low+=1
+        return "" if res == float('inf') else s[start:start+res]
 
-                freq[ord(s[low])] -= 1
-                low += 1
+        
 
-        return "" if res == float('inf') else s[start:start + res]
+        
