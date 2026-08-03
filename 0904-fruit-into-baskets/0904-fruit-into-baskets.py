c386a439e964990e947c7efadc2d57f67dcaac16
Anil Sethy <anilsethy470@gmail.com>
2026-08-03 14:04:06 +0530

    Time: 187 ms (57.05%), Space: 26 MB (34.24%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,17 +1,15 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        left = 0
+        low = 0
         dic = {}
         res = -1
-        for right in range(len(fruits)):
-            dic[fruits[right]] = dic.get(fruits[right],0)+1
+        for high in range(len(fruits)):
+            dic[fruits[high]] = dic.get(fruits[high],0)+1
+
             while(len(dic)>2):
-                dic[fruits[left]]-=1
-                if dic[fruits[left]]==0:
-                    del dic[fruits[left]]
-                left+=1
-
-            res = max(res,right-left+1)
+                dic[fruits[low]]-=1
+                if dic[fruits[low]]==0:
+                    del dic[fruits[low]]
+                low+=1
+            res = max(res,high-low+1)
         return res
-
-        
