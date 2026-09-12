a240e575106f2066313ad67e003aef6423eaa8ff
Anil Sethy <anilsethy470@gmail.com>
2026-09-12 08:48:14 +0530

    Time: 35 ms (46.08%), Space: 21.7 MB (84.64%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,13 +1,13 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        curr_sum = 0
-        count = 0
+        sum = 0
         map = {}
         map[0] = 1
+        count = 0
         for i in range(len(nums)):
-            curr_sum+=nums[i]
-            ques = curr_sum-k
+            sum+=nums[i]
+            ques = sum-k
             freq = map.get(ques,0)
             count+=freq
-            map[curr_sum] = map.get(curr_sum,0)+1
+            map[sum] = map.get(sum,0)+1
         return count
