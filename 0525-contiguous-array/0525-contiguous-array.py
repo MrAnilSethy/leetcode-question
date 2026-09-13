b1bc89ff4cc05882268a791294a41f872b868953
Anil Sethy <anilsethy470@gmail.com>
2026-09-13 22:04:25 +0530

    Time: 94 ms (18.37%), Space: 25.3 MB (50.08%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,11 +1,11 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
+        res = 0
         zero = 0
         one = 0
         map = {}
-        res = 0
-        for i  in range(len(nums)):
-            if nums[i]==0:
+        for i in range(len(nums)):
+            if nums[i] == 0:
                 zero+=1
             else:
                 one+=1
@@ -15,7 +15,8 @@ class Solution:
             elif diff not in map:
                 map[diff] = i
             else:
-                indx = map[diff]
-                size = i - indx
-                res = max(res,size)
+                indx = map.get(diff,0)
+                leng = i - indx
+                res = max(res,leng)
         return res
+        
