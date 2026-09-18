29db1c71d6cbf2413a2925f6a9ccd2c78074ad7b
Anil Sethy <anilsethy470@gmail.com>
2026-09-18 12:14:34 +0530

    Time: 7 ms (5.71%), Space: 21.3 MB (58.11%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,17 +1,16 @@
 class Solution:
     def insert(self, intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
-        intervals.sort()
-        insert = False
+        # insert new interval
         res = []
+        insert = False
         for i in range(len(intervals)):
             st = intervals[i][0]
             if st>=newInterval[0] and insert==False:
                 res.append(newInterval)
-                insert=True
             res.append(intervals[i])
         if insert==False:
             res.append(newInterval)
-        # after insert check overlapping
+        # merge the overlapping
         res.sort()
         st1 = res[0][0]
         end1 = res[0][1]
@@ -19,16 +18,15 @@ class Solution:
         for i in range(1,len(res)):
             st2 = res[i][0]
             end2 = res[i][1]
+            # overlapp
             if end1>=st2:
                 st1 = st1
                 end1 = max(end1,end2)
                 continue
-            # not overlaap
             ans.append([st1,end1])
             st1 = st2
             end1 = end2
         ans.append([st1,end1])
         return ans
-
-
+                
         
