5239463485177c0419c8853a217ab7c862eccc4c
Anil Sethy <anilsethy470@gmail.com>
2026-09-20 09:09:46 +0530

    Time: 527 ms (81.38%), Space: 22.2 MB (82.72%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,25 +4,27 @@ class Solution:
         n = len(nums)
         res = []
         for i in range(0,n-2):
-            if (i> 0 and nums[i]==nums[i-1]):
+            if (i>0 and nums[i]==nums[i-1]):
                 continue
-            sum = -1 * nums[i]
+            s = -1 * nums[i]
             left = i+1
             right = n-1
             while(left<right):
-                if nums[left]+nums[right]==sum:
+                sum = nums[left]+nums[right]
+                if sum==s:
                     res.append([nums[i],nums[left],nums[right]])
                     left+=1
                     right-=1
-                    # left side duplicates
+                    # left side duplicate
                     while(left<n and nums[left]==nums[left-1]):
                         left+=1
-                    # right side duplicates
+                    # right side duplicate
                     while(right>=0 and nums[right]==nums[right+1]):
                         right-=1
-                elif nums[left]+nums[right]<sum:
+                elif sum<s:
                     left+=1
                 else:
                     right-=1
         return res
+
         
