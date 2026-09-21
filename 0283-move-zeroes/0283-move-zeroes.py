04d7321e6d55a4228b93ea7744fc2edcf87e23d5
Anil Sethy <anilsethy470@gmail.com>
2026-09-21 07:43:44 +0530

    Time: 4 ms (59.3%), Space: 20.5 MB (63.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,10 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
         low = 0
         for high in range(len(nums)):
             if nums[high]!=0:
                 nums[low],nums[high]=nums[high],nums[low]
                 low+=1
-     
+            
