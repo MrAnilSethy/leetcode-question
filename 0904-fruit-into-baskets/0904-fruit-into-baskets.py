fdaaf9b3777756424d06eb782554f52bdc483c7d
Anil Sethy <anilsethy470@gmail.com>
2026-09-26 12:46:02 +0530

    Time: 164 ms (85.03%), Space: 25.2 MB (96.11%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,20 +1,18 @@
 class Solution:
-    def totalFruit(self, fruits: List[int]) -> int:
-        low  = 0
+    def totalFruit(self, fruits: list[int]) -> int:
+        low = 0
         dic = {}
-        res = -1
+        max_fruits = float("-inf")
         for high in range(len(fruits)):
             dic[fruits[high]] = dic.get(fruits[high],0)+1
-            while(len(dic)>2):
+            if len(dic)>2:
                 dic[fruits[low]]-=1
                 if dic[fruits[low]]==0:
                     del dic[fruits[low]]
                 low+=1
             size = high-low+1
-            res = max(size,res)
-        return res
-            
-
+            max_fruits = max(max_fruits,size)
+        return max_fruits
 
 
         
