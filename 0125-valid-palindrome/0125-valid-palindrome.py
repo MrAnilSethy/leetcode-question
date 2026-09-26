207ace963c124cb98de875a76bba5b5a4379d3e0
Anil Sethy <anilsethy470@gmail.com>
2026-09-26 08:20:20 +0530

    Time: 8 ms (48.96%), Space: 19.5 MB (94.59%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,17 +3,12 @@ class Solution:
         st = 0
         end = len(s)-1
         while(st<=end):
-            # skip the st side char is not alnum
             while(st<end and not s[st].isalnum()):
                 st+=1
-            # skip the right side char is not alnum
             while(st<end and not s[end].isalnum()):
                 end-=1
             if s[st].lower()!=s[end].lower():
                 return False
             st+=1
             end-=1
-                
         return True
-
-        
